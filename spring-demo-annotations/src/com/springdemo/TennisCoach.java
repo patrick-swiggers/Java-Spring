b855aec425275746630970e756c6dbd9a33a100f
Patrick Swiggers <patrick.swiggers@gmail.com>
2022-05-27 00:24:15 +0200

    IoC and DI with java config

--- spring-demo-annotations/src/com/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/springdemo/TennisCoach.java
@@ -1,12 +1,16 @@
 package com.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("singleton") // or prototype
 public class TennisCoach implements Coach {
 	
 	@Autowired  // Field injection -> "Java Reflection"
+	@Qualifier("happyFortuneService")
 	private FortuneService fortuneService; // from the Interface
 	// Spring will look for any class that implements this interface !!
 	
