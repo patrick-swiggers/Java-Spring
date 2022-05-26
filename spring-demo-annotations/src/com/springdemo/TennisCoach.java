d2b2e38c349dcf3ac8ee0b44bce020858eacf813
Patrick Swiggers <patrick.swiggers@gmail.com>
2022-05-26 15:37:10 +0200

    Autowired

--- spring-demo-annotations/src/com/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/springdemo/TennisCoach.java
@@ -1,14 +1,46 @@
 package com.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
+	
+	@Autowired  // Field injection -> "Java Reflection"
+	private FortuneService fortuneService; // from the Interface
+	// Spring will look for any class that implements this interface !!
+	
+	// define a default constructor
+	public TennisCoach() {
+		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+	/*
+	@Autowired
+	public TennisCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	*/
+	
+	// define a setter method 
+	/*
+	@Autowired
+	public void doSomeCrazyStuff(FortuneService theFortuneService) {
+		System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
+		this.fortuneService = theFortuneService;
+	}
+	*/
 
 	@Override
 	public String getDailyWorkout() {
 		// 
 		return "Practice your backhand volley";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return fortuneService.getFortune();
 	}
 
 }
